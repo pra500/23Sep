0ce8b1c0faf825e113f748ee9694308bfdeca3fe
pragti gupta <pragtig6@gmail.com>
2025-09-23 02:20:00 -0500

    first1

---
+++ src/G.java
@@ -0,0 +1,14 @@
+public class G {
+
+
+    public static void main(String[] args) {
+
+
+        String strr1 = "pragti";
+
+        System.out.println(  strr1.toUpperCase());
+
+
+
+    }
+}
---
+++ src/ddddd.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ddddd {
+
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(2);
+        list.add(9);
+
+        Collections.sort(list);
+       // Collections.reverse(list);
+
+        System.out.println(list);
+
+
+    }
+}
